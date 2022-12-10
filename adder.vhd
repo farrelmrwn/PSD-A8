1c67d4493e136704546811fb2df1f655a1c6e45c
Jeremy Ganda Pandapotan <87458424+JGDoubleP@users.noreply.github.com>
2022-12-10 10:45:29 +0700

    Full_adder Added

--- adder.vhd
+++ adder.vhd
@@ -1,1 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
 
+entity adder is
+    port (
+        
+        a : IN STD_LOGIC;
+        b : IN STD_LOGIC;
+        c_in : IN STD_LOGIC;
+        sum : OUT STD_LOGIC;
+        c_out : OUT STD_LOGIC
+        
+    );
+end entity adder;
+
+architecture rtl of adder is
+
+begin
+
+    c_out <= (a AND b) OR (c_in AND (a XOR b));
+    sum <= a XOR b XOR c_in;
+
+end architecture;
